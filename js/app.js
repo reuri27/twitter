f5b1cd71be773e93b8ff7677cd080be7b182a2c2
Bryan de Asis <asesi27@gmail.com>
2021-01-13 21:58:52 -0400

    modificado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+var url = window.location.href;
+var swLocation = '/twitter/sw.js'
+
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swLocation =  '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
