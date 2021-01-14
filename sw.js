c6dfc11b8fda456746e104aa31ce6d58926f6d72
Bryan de Asis <asesi27@gmail.com>
2021-01-13 22:03:22 -0400

    modificado 2

--- sw.js
+++ sw.js
@@ -12,15 +12,15 @@ const  INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL  = [
     // '/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
     'js/sw-util.js'
 ];
 
